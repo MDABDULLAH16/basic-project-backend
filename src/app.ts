232176a10436480fc9c6b8163b49ff4f5fc9966a
MD ABDULLAH <mdabdullah161036@gmail.com>
2026-05-13 11:27:02 +0600

    notfound handle

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import router from "./routes/routes.js";
 import cors from 'cors'
+import notFound from "./middleware/not-found.js";
 const app: Application = express();
 
 app.use(cors())
@@ -11,6 +12,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello from TypeScript Express!");
 });
 
+app.use(notFound)
 // app.use('',(req:Request,res:Response)=> {
 //     res.send("Route not found")
 // })
---
+++ src/middleware/not-found.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+import { HttpStatus } from "http-status-ts";
+
+const notFound = (req: Request, res: Response, next: NextFunction) => {
+  return res.status(HttpStatus.NOT_FOUND).json({
+    success: false,
+    message: "api not found",
+    error: "",
+  });
+};
+
+export default notFound;
