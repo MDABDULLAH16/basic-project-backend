8389f3ad1b15de5caf505f4962cddfa34855d05f
MD ABDULLAH <mdabdullah161036@gmail.com>
2026-05-13 11:35:04 +0600

    sendResponse reuseable

--- src/app/modules/users/users.controller.ts
+++ src/app/modules/users/users.controller.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from "express";
+ 
+import sendResponse from "../../../utils/sendResponse.js";
 import { HttpStatus } from "http-status-ts";
 
 const createUserReq =async (req:Request,res:Response) => {
     const result = await req.body; 
     console.log(result);
     
-res.status(HttpStatus.CREATED).json({'success': true, message: "User created successfully",data: result });
+  sendResponse(res, {
+    statusCode: HttpStatus.CREATED,
+    success: true,
+    message: "User created successfully.",
+    data: result,
+  });
 };
 
 export const UserController = {
